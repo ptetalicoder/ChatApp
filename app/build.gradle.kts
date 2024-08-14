736201bd33b835125630c6c63ee956dbb4b6fc88
Pranav Tetali <ptetali0801@gmail.com>
2024-08-14 16:29:56 -0500

    Initial commit

---
+++ app/build.gradle.kts
@@ -0,0 +1,11 @@
+apply plugin: 'com.android.application'
+
+android {
+    compileSdkVersion 27
+    defaultConfig {
+        applicationId "com.ptetali.chatapp"
+        minSdkVersion 15
+        targetSdkVersion 27
+
+    }
+}
